c5e10cc461358ef59edd5fcfcfabbab1c4c988f5
Gwen Brinsmead <gwen@Gwens-MacBook-Pro.local>
2014-06-10 06:54:34 -0700

    Initial Commit

---
+++ Comment/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  Comment
+//
+//  Created by Gwen Brinsmead on 6/10/14.
+//  Copyright (c) 2014 Gwen Brinsmead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
